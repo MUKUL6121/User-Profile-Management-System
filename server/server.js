4fd678be9f51c2fd61bb85385d54a5b3d93fb686
MUKUL6121 <148455498+MUKUL6121@users.noreply.github.com>
2025-07-15 10:14:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port= process.env.PORT || 3000;
-require('.env').config();
+require('./.env').config();
 app.use(express.json());
 
 var corsoptions = {
